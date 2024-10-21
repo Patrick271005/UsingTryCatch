a3a6b36295968d29733559e7fc05970d1beac17c
PATRICKFLANAGAN-STUD <G00431255@atu.ie>
2024-10-21 09:31:35 +0100

    Initial set up created

--- src/main/java/UsingTryCatch.java
+++ src/main/java/UsingTryCatch.java
@@ -5,9 +5,22 @@ public class UsingTryCatch {
         int topNum =20;
         int baseNum=0;
         int total=0;
+        System.out.println("Please enter a number between  0 - 9 : ");
         Scanner scan=new Scanner(System.in);
 
-        try
+        //read user input as a String
+        String input = scan.nextLine();
+
+        //Parse the input to an integer
+        int myNum = Integer.parseInt(input);
+
+        //check if the number is between 0 and 9
+        if (myNum >=0 && myNum  <=9)
+        {
+            System.out.println(" You entered " + myNum);
+        }
+
+       /* try
         {
             System.out.println("Please enter a number");
             topNum = scan.nextInt();
@@ -23,6 +36,6 @@ public class UsingTryCatch {
         finally {
             scan.close();
         }
-        System.out.println("The total is " + total);
+        System.out.println("The total is " + total);*/
     }
 }
