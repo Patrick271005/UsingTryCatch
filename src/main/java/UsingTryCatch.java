8b135f03f154224fc0b7441d54ce0c9504151592
PATRICKFLANAGAN-STUD <G00431255@atu.ie>
2024-10-21 09:55:20 +0100

    added retry on error

--- src/main/java/UsingTryCatch.java
+++ src/main/java/UsingTryCatch.java
@@ -2,40 +2,42 @@ import java.util.Scanner;
 
 public class UsingTryCatch {
     public static void main(String[] args) {
-        int topNum =20;
-        int baseNum=0;
-        int total=0;
+
         System.out.println("Please enter a number between  0 - 9 : ");
-        Scanner scan=new Scanner(System.in);
 
-        //read user input as a String
-        String input = scan.nextLine();
 
-        //Parse the input to an integer
-        int myNum = Integer.parseInt(input);
-
-        //check if the number is between 0 and 9
-        if (myNum >=0 && myNum  <=9)
+        try(Scanner scan=new Scanner(System.in))
         {
-            System.out.println(" You entered " + myNum);
+            int myNum = -1;
+            while (myNum <0 || myNum > 9)
+            {
+                String input = scan.nextLine();
+
+                try
+                {
+                    //Parse the input to an integer
+                    myNum = Integer.parseInt(input);
+                    //check if the number is between 0 and 9
+                    if (myNum >=0 && myNum  <=9)
+                    {
+                        System.out.println(" You entered " + myNum);
+                    }
+                    else
+                    {
+                        System.out.println("Not a valid number");
+                    }
+                }
+                catch(NumberFormatException ae)
+                {
+                    System.out.println("retry not a valid number " + ae.getMessage());
+                }
+            }
+            //read user input as a String
+
+
         }
 
-       /* try
-        {
-            System.out.println("Please enter a number");
-            topNum = scan.nextInt();
-            System.out.println("Please enter a  Second number");
-            total=topNum/baseNum;
-            baseNum = scan.nextInt();
-            total = topNum / baseNum;
-        }
-        catch(ArithmeticException ae)
-        {
-            System.out.println("Please enter values greater then 0" + ae.getMessage());
-        }
-        finally {
-            scan.close();
-        }
-        System.out.println("The total is " + total);*/
+
     }
 }
+
